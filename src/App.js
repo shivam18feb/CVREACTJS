a635e0bd83879c3b547752807b8e7c928ab3646b
shivam18feb <shivam18feb@gmail.com>
2023-05-25 15:30:22 +0530

    Navbar page redirect problem resolve

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
  * @returns The App component is being returned, which contains the JSX code for the entire web page.
  */
 import "./App.css";
-import React from "react";
+
 import Navbar from "./components/Navbar";
 import Intro from "./components/Intro";
 import Education from "./components/Education";
@@ -12,41 +12,48 @@ import Project from "./components/Project";
 import Contactform from "./components/Contactform";
 import Footer from "./components/Footer";
 import img1 from "./images/profilepic.jpg";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 function App() {
   return (
     <>
-      <body>
-        <div className="navcolor">
-        <Navbar />
-        </div>
-        <div className="mainpostion">
-          <div className="postion">
-            <img src={img1} alt="" />
+      
+        <body>
+          <div className="navcolor">
+          <Router>
+            <Navbar />
+              {/* Your routes here */}
+          </Router>
           </div>
-          <div className="postion2">
-            <Intro />
+          <div className="mainpostion">
+            <div className="postion">
+              <img src={img1} alt="" />
+            </div>
+            <div className="postion2">
+              <Intro />
+            </div>
           </div>
-        </div>
-        <div className="container">
-          <Education />
-        </div>
-        <div className="container-mb-3">
-          <div className="projecthead">
-            <h1>Our Projects And Discription ... </h1>
+          <div className="container">
+            <Education />
           </div>
-          <Project />
-        </div>
+          <div className="container-mb-3">
+            <div className="projecthead">
+              <h1>Our Projects And Discription ... </h1>
+            </div>
+            <Project />
+          </div>
 
-        <div className="container" id="adjust">
-          <h1>Contact us for Hireing me ..</h1>
-          <Contactform />
-        </div>
-        <div>
-          <Footer />
-        </div>
-      </body>
+          <div className="container" id="adjust">
+            <h1>Contact us for Hireing me ..</h1>
+            <Contactform />
+          </div>
+          <div>
+            <Footer />
+          </div>
+        </body>
+      
     </>
   );
 }
-
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,115 +2,62 @@
  * The function returns a React component for a navbar with links and a message.
  * @returns A functional component for a navbar in React.
  */
+
 import React from "react";
-import { Routes , Route , useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const navigate = useNavigate();
 
-  const navigateToExperience =() =>{
-    navigate('/experience');
-  };
-  const navigateToProjects =() =>{
-    navigate('/projects');
-  };
-  const navigateToEducation =() =>{
-    navigate('/education');
-  };
-  const navigateToSkills =() =>{
-    navigate('/skills');
-  };
-  const navigateToContact =() =>{
-    navigate('/contact');
-  };
-  const navigateToAbout =() =>{
-    navigate('/about');
-  };
   return (
-   
     <div>
-      <nav
-        className="navbar navbar-expand-lg navbar-light bg-light"
-        id="fitnav"
-      >
+      <nav className="navbar navbar-expand-lg navbar-light bg-light" id="fitnav">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <a className="navbar-brand" onClick={() => navigate("/")}>
             Shivam Curriculum vitae
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="/navbarText"
-            aria-controls="navbarText"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0" id="navbarCenter">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href={navigateToExperience}>
-                  Experience
-                </a>
-                <Routes>
-                  <Route path="/experience" element={<experience/>} />
-                </Routes>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href={navigateToProjects}>
-                  Projects
-                </a>
-                <Routes>
-                  <Route path="/" element={<projects/>}/>
-                </Routes>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href={navigateToEducation}>
-                  Education
-                </a>
-                <Routes>
-                  <Route path="/" element={<education/>}/>
-                </Routes>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href={navigateToSkills}>
-                  Skills
-                </a>
-                <Routes>
-                  <Route path="/" element={<skills/>}/>
-                </Routes>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href={navigateToAbout}>
-                  About Us
-                </a>
-                <Routes>
-                  <Route path="/" element={<about/>}/>
-                </Routes>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href={navigateToContact}>
-                  Contact Us
-                </a>
-                <Routes>
-                  <Route path="/" element={<contact/>}/>
-                </Routes>
-              </li>
-            </ul>
-            <span className="navbar-text">
-              My All Skills and Projects are show here!!
-            </span>
-          </div>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0" id="navbarCenter">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" onClick={() => navigate("/")}>
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" onClick={() => navigate("/experience")}>
+                Experience
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" onClick={() => navigate("/projects")}>
+                Projects
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" onClick={() => navigate("/education")}>
+                Education
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" onClick={() => navigate("/skills")}>
+                Skills
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" onClick={() => navigate("/about")}>
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" onClick={() => navigate("/contact")}>
+                Contact Us
+              </a>
+            </li>
+          </ul>
+          <span className="navbar-text">
+            My All Skills and Projects are show here!!
+          </span>
         </div>
       </nav>
     </div>
-
   );
 }
